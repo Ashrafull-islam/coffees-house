76ccf708eaa993720d0eb11ab564d4776af317c9
Ashrafull islam <mmmdashrafull211@gmail.com>
2025-07-04 01:16:26 +0600

    ultimate

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -29,25 +29,6 @@ export async function GET(request: NextRequest) {
 }
 
 // DELETE /api/product/:id
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  try {
-    await connectionToDatabase();
-
-    const deletedUser = await User.findByIdAndDelete(params.id);
-
-    if (!deletedUser) {
-      return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
-    }
-
-    return NextResponse.json({ success: true, message: "User deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting user:", error);
-    return NextResponse.json({ success: false, message: "Failed to delete user" }, { status: 500 });
-  }
-}
 
 export async function PUT(request: NextRequest) {
   try {
